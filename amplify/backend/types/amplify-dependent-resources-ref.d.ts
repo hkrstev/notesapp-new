312d2dc422d0e3fa712a63a24ce821f8f99501fc
hkrstev <103205287+hkrstev@users.noreply.github.com>
2023-04-11 12:58:19 -0500

    api working in console

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notesappnew": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
