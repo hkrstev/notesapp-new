28bbf23b8f12bd3785cac3de8ece0830b9fe7040
hkrstev <103205287+hkrstev@users.noreply.github.com>
2023-04-11 13:02:53 -0500

    app now working properly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,206 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useReducer} from 'react';
+import { API } from 'aws-amplify';
+import { List } from 'antd';
+import 'antd/dist/reset.css';
+import { listNotes } from './graphql/queries';
 
-function App() {
+import { v4 as uuid } from 'uuid';
+import { Input, Button } from 'antd';
+
+import { createNote as CreateNote,  deleteNote as DeleteNote, updateNote as UpdateNote } from './graphql/mutations';
+
+import { onCreateNote } from './graphql/subscriptions'
+
+const CLIENT_ID = uuid()
+
+
+
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_NOTES':
+      return { ...state, notes: action.notes, loading: false }
+    case 'ERROR':
+      return { ...state, loading: false, error: true }
+    default:
+      return state
+
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes]}
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } }
+  }
+}
+
+
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onChange = (e) => {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
+  }
+
+  const createNote = async() => {
+    const { form } = state
+    if (!form.name || !form.description) {
+       return alert('please enter a name and description')
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log('successfully created note!')
+    } catch (err) {
+      console.log("error: ", err)
+    }
+  }
+
+  const deleteNote = async({ id }) => {
+    const index = state.notes.findIndex(n => n.id === id)
+    const notes = [
+      ...state.notes.slice(0, index),
+      ...state.notes.slice(index + 1)];
+    dispatch({ type: 'SET_NOTES', notes })
+    try {
+      await API.graphql({
+        query: DeleteNote,
+        variables: { input: { id } }
+      })
+      console.log('successfully deleted note!')
+      } catch (err) {
+        console.log({ err })
+    }
+  }
+
+  const updateNote = async (note) => {
+    const index = state.notes.findIndex(n => n.id === note.id)
+  const notes = [...state.notes]
+  notes[index].completed = !note.completed
+  dispatch({ type: 'SET_NOTES', notes})
+  try {
+    await API.graphql({
+      query: UpdateNote,
+      variables: { input: { id: note.id, completed: notes[index].completed } }
+    })
+    console.log('note successfully updated!')
+  } catch (err) {
+    console.log('error: ', err)
+  }
+  };
+  const fetchNotes = async() => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      })
+      const sortedNotes = notesData.data.listNotes.items.sort((a, b) => a.name.localeCompare(b.name))
+      dispatch({ type: 'SET_NOTES', notes: sortedNotes })
+    } catch (err) {
+      console.log('error: ', err)
+      dispatch({ type: 'ERROR' })
+    }
+  }
+
+  const numCompletedNotes = state.notes.filter(note => note.completed).length;
+  const totalNotes = state.notes.length;
+
+  useEffect(() => {
+    fetchNotes()
+    const subscription = API.graphql({
+      query: onCreateNote
+    })
+      .subscribe({
+        next: noteData => {
+          const note = noteData.value.data.onCreateNote
+          if (CLIENT_ID === note.clientId) return
+          dispatch({ type: 'ADD_NOTE', note })
+        }
+      })
+      return () => subscription.unsubscribe()
+  }, [])
+
+  const renderItem = (item) => {
+    return (
+      
+      <List.Item
+  style={styles.item}
+  actions={[
+    <p style={styles.p} onClick={() => deleteNote(item)}>Delete</p>,
+    <p style={styles.p} onClick={() => updateNote(item)}>
+        {item.completed ? 'completed' : 'mark completed'}
+    </p>
+  ]}
+>
+  <List.Item.Meta
+   title={item.name}
+   description={item.description}
+  />
+</List.Item>
+    )
+  }
+
+  const styles = {
+    container: {padding: 20},
+    input: {marginBottom: 10},
+    item: { textAlign: 'left' },
+    p: { color: '#1890ff' }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+    <div style={styles.container}>
+    
+      <Input
+  onChange={onChange}
+  value={state.form.name}
+  placeholder="Note Name"
+  name='name'
+  style={styles.input}
+/>
+<Input
+  onChange={onChange}
+  value={state.form.description}
+  placeholder="Note description"
+  name='description'
+  style={styles.input}
+/>
+
+<Button
+  onClick={createNote}
+  type="primary"
+>Create Note</Button>
+    <hr/>
+      <h2>
+        {state.loading ? 'Loading Notes...' : 'Here are your notes...'}
+      </h2>
+    <hr/>
+
+    <hr/>
+    
+      <h3>
+      <span>{`Completed ${numCompletedNotes} of ${totalNotes} notes`}</span>
+      </h3>
+    <hr/>
+    <List
+      loading={state.loading}
+      dataSource={state.notes}
+      renderItem={renderItem}
+    />
+  </div>
   );
 }
 
